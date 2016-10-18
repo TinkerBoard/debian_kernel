80abb4b3c9a802473b3d0520194e40d5930822a2
Jacob Chen <jacob2.chen@rock-chips.com>
2016-10-18 10:05:35 +0800

    UNMERGED FIX: fix cif isp10 build
    Change-Id: I561fe2db1a38bafcf56db7e8991172d6031da41a
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>

--- drivers/media/platform/rk-isp10/cif_isp10_rk3288.c
+++ drivers/media/platform/rk-isp10/cif_isp10_rk3288.c
@@ -13,7 +13,7 @@
  * (at your option) any later version.
 **************************************************************************
  */
-#ifdef CONFIG_ARM64
+#ifndef CONFIG_ARM64
 #include <linux/init.h>
 #include <linux/delay.h>
 #include <linux/regmap.h>
