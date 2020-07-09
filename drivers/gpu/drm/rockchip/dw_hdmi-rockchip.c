5796788b85f5d47fb63c0ae50548d05d82c54770
Caesar Wang <wxt@rock-chips.com>
2020-07-09 16:38:48 +0800

    drm/rockchip: fixes atomic for dw_hdmi-rockchip driver
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    Change-Id: I158496f270cd31f3853c8f2a3f29be71d4828007

--- drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
+++ drivers/gpu/drm/rockchip/dw_hdmi-rockchip.c
@@ -17,6 +17,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/phy/phy.h>
 
+#include <drm/drm_atomic.h>
 #include <drm/drm_of.h>
 #include <drm/drmP.h>
 #include <drm/drm_crtc_helper.h>
@@ -1086,9 +1087,33 @@ dw_hdmi_rockchip_set_property(struct drm_connector *connector,
 	} else if (property == hdmi->quant_range) {
 		hdmi->hdmi_quant_range = val;
 		return 0;
-	} else {
-		return drm_atomic_helper_connector_set_property(connector, property, val);
 	}
+
+	if (property != config->prop_crtc_id &&
+	    property != connector->content_protection_property &&
+	    property != config->tv_select_subconnector_property &&
+	    property != config->tv_left_margin_property &&
+	    property != config->tv_right_margin_property &&
+	    property != config->tv_top_margin_property &&
+	    property != config->tv_bottom_margin_property &&
+	    property != config->tv_mode_property &&
+	    property != config->tv_brightness_property &&
+	    property != config->tv_contrast_property &&
+	    property != config->tv_flicker_reduction_property &&
+	    property != config->tv_overscan_property &&
+	    property != config->tv_saturation_property &&
+	    property != config->tv_hue_property &&
+	    property != config->hdr_source_metadata_property) {
+		DRM_ERROR("failed to set rockchip hdmi connector property\n");
+		return -EINVAL;
+	}
+
+	if (!state)
+		return drm_atomic_helper_connector_set_property(connector,
+								property, val);
+	else
+		return drm_atomic_connector_set_property(connector, state,
+							 property, val);
 }
 
 static int
