aff161a7994f85e502804870035721cbf46c9d77
bross1_kuo <bross1_kuo@asus.com>
2021-08-25 10:25:30 +0800

    Revert "drm/rockchip: framebuffer: Add support /dev/graphics/fb0 read"
    This reverts commit 726122dc14c6bbde2cc2c351b5eaf45257307003.
    
    Change-Id: If6c3dfa6563129142af19199d02187a38408148c

--- drivers/gpu/drm/rockchip/rockchip_drm_fbdev.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_fbdev.c
@@ -69,8 +69,6 @@ static struct fb_ops rockchip_drm_fbdev_ops = {
 	.fb_set_par	= drm_fb_helper_set_par,
 	.fb_blank	= rockchip_fbdev_blank,
 	.fb_pan_display	= drm_fb_helper_pan_display,
-	.fb_read	= drm_fb_helper_sys_read,
-	.fb_write	= drm_fb_helper_sys_write,
 	.fb_setcmap	= drm_fb_helper_setcmap,
 	.fb_dmabuf_export	= rockchip_fbdev_get_dma_buf,
 };
